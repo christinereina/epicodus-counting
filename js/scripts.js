d0204a1c44e3c4b4771e089def6523138c3c2694
Christine Augustine and jeremy kale padot <jeremy@>
2020-01-22 10:47:09 -0800

    add a completed looping for exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
  $(document).ready(function() {
+    $("form.counter").submit(function(event) {
+    event.preventDefault();  
+    var countTo = parseInt($("input#countTo").val());
+    var countBy = parseInt($("input#countBy").val());
+    console.log(countBy);
+
+    for (var index = 0; index <= countTo; index += countBy) {
+       $("ul").append("<li>" + index + "</li>");
+
+
+ 
+    }
 
  });
- 
+});
+
